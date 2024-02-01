29bd673d9c728a7c632606900dd271bea1ee6131
Cesar <cesarmdiaz98@gmail.com>
2024-02-02 00:11:46 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Cesar Montero Diaz on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
